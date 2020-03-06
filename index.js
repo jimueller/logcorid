1ed4612c3ed71b83fa40b337da8cd48f4e312d9d
Jim Mueller <jim@jimueller.com>
2020-03-06 13:07:37 -0600

    Fixes header name

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const express = require('express');
-const rTracer = require('cls-rtracer');
-const logger = require('./logger.js');
+const express = require("express");
+const rTracer = require("cls-rtracer");
+const logger = require("./logger.js");
 
 const app = express();
 
-app.use(rTracer.expressMiddleware({
-  useHeader: true,
-  headerName: 'X--CORRELATION-ID'
-}));
+app.use(
+  rTracer.expressMiddleware({
+    useHeader: true,
+    headerName: "X-CORRELATION-ID"
+  })
+);
 
-app.get('/api/v1/cats/:id', (req, res, next) => {
+app.get("/api/v1/cats/:id", (req, res, next) => {
   logger.info(`asked for cat id: ${req.params.id}`);
   res.json({
     correlationId: rTracer.id(),
@@ -17,4 +19,4 @@ app.get('/api/v1/cats/:id', (req, res, next) => {
   });
 });
 
-app.listen(3000, () => logger.info('Listening on 3000'));
+app.listen(3000, () => logger.info("Listening on 3000"));
